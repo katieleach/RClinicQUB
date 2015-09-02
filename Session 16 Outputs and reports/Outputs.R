2b2b177311e34e4a290d14385c9f81fe85350cc2
Katie Leach <kleach01@qub.ac.uk>
2015-09-02 09:09:27 +0100

    Add Session 16

---
+++ Session 16 Outputs and reports/Outputs.R
@@ -0,0 +1,68 @@
+####################
+###Outputs from R###
+####################
+
+### Writing out data files
+#First let's create some data
+
+x <- rnorm(30)
+y <- rnorm(30)
+z <- rnorm(30)
+
+data.xyz <- data.frame(x, y, z)
+
+write.csv(data.xyz, 'q:/data.xyz.csv', row.names=FALSE) #writes csv file
+
+write.table(data.xyz, 'q:/data.xyz.txt', row.names=FALSE) #writes text file
+
+
+### Create text file containing model summary
+
+library(nlme)
+library(spdep)
+
+install.packages('RANN')
+
+#load wheat2 data
+data(Wheat2)
+
+#create neighbourhood object using k nearest neighbours
+
+neigh <- knn2nb(knearneigh(cbind(Wheat2$latitude, Wheat2$longitude)))
+
+#use lm with spatial errors
+
+mod.sp <- spautolm(yield ~ variety + Block, data=Wheat2, listw=nb2listw(neigh), family='SAR')
+
+summary(mod.sp)
+
+out <- capture.output(summary(mod.sp))
+
+cat(out,file="q:/out1.txt",sep="\n",append=TRUE)
+
+#cat(out, file='c:/Users/Hannah/Documents/R Clinic/Session 16 Outputs/out1.txt', sep="\n", append=TRUE)
+
+
+####Outputting graphics
+
+#we're going to use the mtcars data for an example of how to output a pretty plot
+install.packages('corrplot')
+library(corrplot)
+
+data(mtcars)
+
+cor.cars <- cor(mtcars, method='pearson')
+corrplot(cor.cars)
+
+#jpeg('c:/Users/Hannah/Documents/R Clinic/Session 16 Outputs/corr.jpg', width=480, height=480,units='px')
+
+jpeg('q:/corr.jpg', width=480, height=480, units='px')
+
+corrplot(cor.cars)
+dev.off()
+
+
+
+
+
+
